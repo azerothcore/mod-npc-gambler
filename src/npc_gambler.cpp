adc2abeeefb9d608a0f5836b1bf4b9085bc6103d
Benjamin Jackson <38561765+benjymansy123@users.noreply.github.com>
2022-02-04 00:11:02 -0500

    Update npc_gambler.cpp

--- src/npc_gambler.cpp
+++ src/npc_gambler.cpp
@@ -504,7 +504,7 @@ public:
                     {
                     case 1:
                     {
-                        me->MonsterSay("Come one, come all! Step right up to Skinny's! Place your bets, Place your bets!", LANG_UNIVERSAL, NULL);
+                        me->Say("Come one, come all! Step right up to Skinny's! Place your bets, Place your bets!", LANG_UNIVERSAL, NULL);
                         me->HandleEmoteCommand(EMOTE_ONESHOT_EXCLAMATION);
 
                         if (GamblerEmoteSpell != 0)
@@ -517,21 +517,21 @@ public:
                     }
                     case 2:
                     {
-                        me->MonsterSay("Come on! Place your bets, Don't be a chicken!", LANG_UNIVERSAL, NULL);
+                        me->Say("Come on! Place your bets, Don't be a chicken!", LANG_UNIVERSAL, NULL);
                         me->HandleEmoteCommand(EMOTE_ONESHOT_CHICKEN);
                         MessageTimer = urand(60000, 180000);
                         break;
                     }
                     case 3:
                     {
-                        me->MonsterSay("Don't make me sad, Come and gamble! Step right up and win today!", LANG_UNIVERSAL, NULL);
+                        me->Say("Don't make me sad, Come and gamble! Step right up and win today!", LANG_UNIVERSAL, NULL);
                         me->HandleEmoteCommand(EMOTE_ONESHOT_CRY);
                         MessageTimer = urand(60000, 180000);
                         break;
                     }
                     default:
                     {
-                        me->MonsterSay("Come one, come all!Step right up to Skinny's! Place your bets, Place your bets!", LANG_UNIVERSAL, NULL);
+                        me->Say("Come one, come all!Step right up to Skinny's! Place your bets, Place your bets!", LANG_UNIVERSAL, NULL);
                         me->HandleEmoteCommand(EMOTE_ONESHOT_EXCLAMATION);
                         me->CastSpell(me, 44940);
                         MessageTimer = urand(60000, 180000);
